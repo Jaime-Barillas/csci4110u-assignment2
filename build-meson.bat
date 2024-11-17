4a24c9fa110ef0c513b7ac04bd3d01da1af46797
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2024-11-17 14:48:58 -0500

    fix meson configure commads

--- build-meson.bat
+++ build-meson.bat
@@ -1,5 +1,5 @@
 meson setup --vsenv build
-meson configure -C build -Dspng:default_library=shared
-meson configure -C build -Dspng:static_zlib=false
-meson configure -C build -Dzlib:default_library=shared
+meson configure build -Dspng:default_library=shared
+meson configure build -Dspng:static_zlib=false
+meson configure build -Dzlib:default_library=shared
 meson compile -C build
