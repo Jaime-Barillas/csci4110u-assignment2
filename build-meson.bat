607204d00b4de784ec6d2ae631b8b69abf9d4473
Jaime Barillas <36712691+Jaime-Barillas@users.noreply.github.com>
2024-11-17 14:52:50 -0500

    fix

--- build-meson.bat
+++ build-meson.bat
@@ -3,3 +3,7 @@ meson configure build -Dspng:default_library=shared
 meson configure build -Dspng:static_zlib=false
 meson configure build -Dzlib:default_library=shared
 meson compile -C build
+
+copy build\assignment2.exe build-win\
+copy build\subprojects\libspng-0.7.4\spng-0.dll build-win\
+copy build\subprojects\zlib-1.2.13\z.dll build-win\
